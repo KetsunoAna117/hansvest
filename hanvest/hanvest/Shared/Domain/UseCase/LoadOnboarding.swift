0ddd77a87c9511acc3dc24c3a4c28952a379d35b
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2024-10-25 01:41:58 +0700

    feat: create loadonboarding class and protocol (no content yet)

---
+++ hanvest/hanvest/Shared/Domain/UseCase/LoadOnboarding.swift
@@ -0,0 +1,14 @@
+//
+//  LoadShowOnboarding.swift
+//  hanvest
+//
+//  Created by Bryan Vernanda on 24/10/24.
+//
+
+protocol LoadOnboarding {
+    
+}
+
+struct LoadOnboardingImpl: LoadOnboarding {
+    
+}
