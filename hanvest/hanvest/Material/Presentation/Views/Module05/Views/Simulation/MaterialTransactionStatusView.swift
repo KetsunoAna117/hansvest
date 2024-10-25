7904339df83d6e177b7e5a4c8bc4d3925bc3efec
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-10-25 11:09:58 +0700

    Create Material Transaction Status View

---
+++ hanvest/hanvest/Material/Presentation/Views/Module05/Views/Simulation/MaterialTransactionStatusView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialTransactionStatusView.swift
+//  hanvest
+//
+//  Created by Hans Arthur Cupiterson on 25/10/24.
+//
+
+import SwiftUI
+
+struct MaterialTransactionStatusView: View {
+    let moduleRouter: any Module05RouterProtocol
+    let transaction: TransactionStatusViewModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MaterialTransactionStatusView()
+}
