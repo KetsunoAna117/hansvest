4fdaebb3f6be7f64fdb35a91b4442ca6bdee70b0
Bryan Vernanda <91310099+13ry4nV3rn4nd4@users.noreply.github.com>
2024-10-25 11:05:31 +0700

    Han 173 fe create a function to navigate user to onboarding if the user is new DONE!!! (#89)
    * feat: create loadonboarding class and protocol (no content yet)
    
    * feat: create content of LoadOnboarding
    
    * feat: finished creating a function to navigate user to onboarding

--- hanvest/hanvest/Onboarding/Presentation/Components/Views/HanvestRiskProfileOpeningView.swift
+++ hanvest/hanvest/Onboarding/Presentation/Components/Views/HanvestRiskProfileOpeningView.swift
@@ -9,28 +9,26 @@ import SwiftUI
 
 struct HanvestRiskProfileOpeningView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 85) {
-                VStack(spacing: 8) {
-                    Text("Welcome to Hanvesting")
-                        .font(.nunito(.title1, .bold))
-                        .frame(maxWidth: .infinity)
-                    
-                    Text("Ready to level up?\nFill in your risk profile\nto personalize your learning path")
-                        .font(.subheadline)
-                        .frame(maxWidth: .infinity)
-                }
-                .frame(maxWidth: .infinity)
+        VStack(spacing: 85) {
+            VStack(spacing: 8) {
+                Text("Welcome to Hanvesting")
+                    .font(.nunito(.title1, .bold))
+                    .frame(maxWidth: .infinity)
                 
-                Image("onboarding-hanvest-app-logo")
+                Text("Ready to level up?\nFill in your risk profile\nto personalize your learning path")
+                    .font(.subheadline)
+                    .frame(maxWidth: .infinity)
             }
             .frame(maxWidth: .infinity)
-            .multilineTextAlignment(.center)
+            
+            Image("onboarding-hanvest-app-logo")
         }
-        .padding(.horizontal, 37)
+        .frame(maxWidth: .infinity)
+        .multilineTextAlignment(.center)
     }
 }
 
 #Preview {
     HanvestRiskProfileOpeningView()
+        .padding(.horizontal, 20)
 }
