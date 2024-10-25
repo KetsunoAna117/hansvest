a0d194c45c549236c8d41d07295ddc1c3c698950
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2024-10-25 10:53:29 +0700

    feat: finished creating a function to navigate user to onboarding

--- hanvest/hanvest/Onboarding/Presentation/ViewModels/RiskProfileViewModel.swift
+++ hanvest/hanvest/Onboarding/Presentation/ViewModels/RiskProfileViewModel.swift
@@ -7,14 +7,51 @@
 
 import Foundation
 
-@Observable
-class RiskProfileViewModel {
-    var resultState: RiskProfileType = .conservative
-    var userSelectedAnswers = Array(repeating: "", count: RiskProfileQuestionsAndOptions.allCases.count)
+class RiskProfileViewModel: ObservableObject {
+    let progressBarMinValue: Int = 0
+    let progressBarMaxValue: Int = 100
+    
+    var resultState: RiskProfileType = .conservative // will be implemented later
+    
+    @Published var pageState: RiskProfilePageState = .pageOpening
+    @Published var currentTab: Int = 0
+    @Published var progressBarCurrValue: Int = 4
+    @Published var userSelectedAnswers = Array(repeating: "", count: RiskProfileQuestionsAndOptions.allCases.count)
+    
     
     func calculateRiskProfile() -> RiskProfileType {
         // add logic to calculate risk profile from the 'userSelectedAnswers' variable
         
         return .conservative
+    }
+    
+    func goToNextPage(router: any AppRouterProtocol) {
+        if currentTab < RiskProfilePageState.pageRiskResult.rawValue {
+            if !checkIsDisabled() {
+                currentTab += 1
+                updateProgressBarValue()
+            }
+        } else {
+            router.popToRoot()
+            router.startScreen = .main
+        }
+    }
+    
+    func changePageState() {
+        if currentTab < RiskProfilePageState.pageRiskResult.rawValue {
+            pageState = .pageQuestion
+        } else {
+            pageState = .pageRiskResult
+        }
+    }
+    
+    func updateProgressBarValue() {
+        if pageState == .pageQuestion {
+            progressBarCurrValue += (progressBarMaxValue / RiskProfileQuestionsAndOptions.allCases.count)
+        }
+    }
+    
+    func checkIsDisabled() -> Bool {
+        return pageState == .pageQuestion && userSelectedAnswers[currentTab - 1].isEmpty
     }
 }
