814d4db03613186da0137daed41f49a567a4a884
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2024-10-26 15:15:02 +0700

    feat: create tipkit class for all available tipkit based on figma is done

---
+++ hanvest/hanvest/Material/Presentation/Views/Main/Tip/MainView-Placeholder-TipKit.swift
@@ -0,0 +1,36 @@
+//
+//  MainView-Placeholder-TipKit.swift
+//  hanvest
+//
+//  Created by Bryan Vernanda on 26/10/24.
+//
+
+import TipKit
+
+struct Module06ThisIsNotificationTip: Tip {
+    var title: Text {
+        Text("This is Notification")
+    }
+    
+    var message: Text {
+        Text("You can see new news here.")
+    }
+    
+    var asset: Image {
+        Image(systemName: "chart.bar.horizontal.page")
+    }
+}
+
+struct Module02VirtualBalanceTip: Tip {
+    var title: Text {
+        Text("Virtual Balance")
+    }
+    
+    var message: Text {
+        Text("A digital token that can be used to buy stocks and virtual items like accessories.")
+    }
+    
+    var asset: Image {
+        Image(systemName: "creditcard")
+    }
+}
--- hanvest/hanvest/Material/Presentation/Views/Module06/Tip/Placeholder-Tipkit.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Placeholder-Tipkit.swift
-//  hanvest
-//
-//  Created by Hans Arthur Cupiterson on 26/10/24.
-//
-
