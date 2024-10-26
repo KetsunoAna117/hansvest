bd59010c2b0e7d052a5dc9924ba424cc42f7cfbd
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-10-26 21:02:26 +0700

    Create Module06 Screen View

---
+++ hanvest/hanvest/Material/Presentation/Views/Module06/Views/MaterialModule06ScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  MaterialModule06ScreenView.swift
+//  hanvest
+//
+//  Created by Hans Arthur Cupiterson on 26/10/24.
+//
+
+import SwiftUI
+
+struct MaterialModule06ScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MaterialModule06ScreenView()
+}
