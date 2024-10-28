bab8b6a1731b84e8919f52e53908b202be29e77b
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2024-10-28 09:35:15 +0700

    feat: trying every possibilities for tipkit features

--- hanvest/hanvest/Material/Presentation/Views/Module05/Views/Tip/TestTip.swift
+++ hanvest/hanvest/Material/Presentation/Views/Module05/Views/Tip/TestTip.swift
@@ -19,6 +19,8 @@ enum TestTip: Tip {
     @Parameter
     static var showThird: Bool = false
     
+    static let eventCount = Event(id: "eventCount")
+    
     // Provide a unique ID for each tip
     var id: String {
         switch self {
@@ -38,9 +40,12 @@ enum TestTip: Tip {
     
     var rules: [Rule] {
         switch self {
-        case .first: return [#Rule(Self.$showFirst) { $0 == true }]
-        case .second: return [#Rule(Self.$showSecond) { $0 == true }]
-        case .third: return [#Rule(Self.$showThird) { $0 == true }]
+        case .first: return [#Rule(Self.eventCount) { event in
+            event.donations.count == 0 }]
+        case .second: return [#Rule(Self.eventCount) { event in
+            event.donations.count == 1 }]
+        case .third: return [#Rule(Self.eventCount) { event in
+            event.donations.count == 2 }]
         }
     }
     var shouldDisplay: Bool {
@@ -55,38 +60,192 @@ enum TestTip: Tip {
     }
 }
 
-struct TestView: View {
-    var body: some View {
-        VStack {
-            Text("First tip")
-                .popoverTip(TestTip.first)
-                .onTapGesture {
-                    TestTip.showSecond = true
-                }
-            Spacer()
-            Text("Second tip")
-                .popoverTip(TestTip.second)
-                .onTapGesture {
-                    TestTip.showThird = true
-                }
-            Spacer()
-            Text("Third tip")
-                .popoverTip(TestTip.third)
+struct PopoverTip1: Tip {
+
+    var title: Text {
+        Text("Test title 1").foregroundStyle(.indigo)
+    }
+    var rules: [Rule] {
+        #Rule(TestView.tipDidOpen) {
+            $0.donations.count == 0
         }
-        .onAppear(perform: {
-            TestTip.showFirst = true
-        })
+    }
+    var message: Text? {
+        Text("Test message 1")
     }
 }
 
-#Preview {
-    TestView()
-        .task {
-            try? Tips.resetDatastore() // for debugging
-            try? Tips.configure([
-                .displayFrequency(.immediate),
-                .datastoreLocation(.applicationDefault)
-            ])
+struct PopoverTip2: Tip {
+    static let id: Int = 2
+    var title: Text {
+        Text("Test title 2").foregroundStyle(.indigo)
+    }
+    var rules: [Rule] {
+        #Rule(TestView.tipDidOpen) {
+            $0.donations.count == 1 //Odd
         }
+    }
+
+    var message: Text? {
+        Text("Test message 2")
+    }
+}
+
+struct TestView: View {
+    static let tipDidOpen = Tips.Event(id: "tipDidOpen")
+//    @State private var timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        VStack {
+//            TipView(PopoverTip1())
+//               .tipViewStyle(TipWithoutDismissViewStyle())
+//               .interactiveDismissDisabled(false)
+            Text("Tip 1")
+                .popoverTip(PopoverTip1())
+                .interactiveDismissDisabled(false)
+                .onTapGesture {
+                    // Explicitly invalidate the tip when tapped
+//                    PopoverTip1().invalidate(reason: .actionPerformed)
+
+                    // After invalidating, send an event to update rules for other tips
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        TestView.tipDidOpen.sendDonation()
+                    }
+                }
+//
+//            Text("Tip 2")
+//                .popoverTip(PopoverTip2())
+//                .onTapGesture {
+//                    // Explicitly invalidate the tip when tapped
+//                    PopoverTip2().invalidate(reason: .actionPerformed)
+//
+//                    // After invalidating, send an event to update rules for other tips
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//                        TestView.tipDidOpen.sendDonation()
+//                    }
+//                }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+enum FeedTip: String, Tip {
+    case welcome
+    case add
+    case remove
+    
+    var id: String {
+        rawValue
+    }
+    
+    var title: Text {
+        switch self {
+        case .welcome:
+            Text("Welcome")
+        case .add:
+            Text("Add")
+        case .remove:
+            Text("Remove")
+        }
+    }
+}
+
+struct FeedView: View {
+    @State private var tips = TipGroup(.ordered) {
+        [
+            FeedTip.welcome,
+            .add,
+            .remove
+        ]
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                TipView(
+                    tips.currentTip?.id == FeedTip.welcome.id ? tips.currentTip : nil
+                )
+                
+                Text(verbatim: "Item")
+                    .popoverTip(
+                        tips.currentTip?.id == FeedTip.remove.id ? tips.currentTip : nil
+                    )
+            }
+            .navigationTitle("Items")
+            .toolbar {
+                Button("Add") {
+                    FeedTip.add.invalidate(reason: .actionPerformed)
+                    // Add another item...
+                }
+                .popoverTip(
+                    tips.currentTip?.id == FeedTip.add.id ? tips.currentTip : nil
+                )
+            }
+        }
+    }
+}
+
+struct AbsolutePositionReader: View {
+  let id: String?
+  init() { self.id = nil }
+  init(_ id: String?) { self.id = id }
+  
+  var body: some View {
+    GeometryReader { metrics in
+      let absolutePosition = CGPoint(
+        x: metrics.frame(in: .named("AbsolutePositionReaderSpace")).midX, // use min/mid/max as needed
+        y: metrics.frame(in: .named("AbsolutePositionReaderSpace")).midY  // use min/mid/max as needed
+      )
+
+      Rectangle()
+        .fill(Color.clear)
+        .preference(
+          key: AbsolutePositionKey.self,
+          value: [AbsolutePositionValue(id: id, absolutePosition: absolutePosition)]
+        )
+    }
+  }
+}
+
+struct AbsolutePositionValue: Equatable {
+  let id: String?
+  let absolutePosition: CGPoint
+}
+
+struct AbsolutePositionKey: PreferenceKey {
+    typealias Value = [AbsolutePositionValue]
+    static var defaultValue: [AbsolutePositionValue] = []
+    static func reduce(value: inout [AbsolutePositionValue], nextValue: () -> [AbsolutePositionValue]) {
+        value.append(contentsOf: nextValue())
+    }
+}
+
+struct Lines: View {
+    let testo : String = "There is a thunderstorm in the area. Added some testing long text to demo that wrapping works correctly!"
+
+
+    var body: some View {
+        hilightedText(str: testo, searched: "thunderstorm")
+            .multilineTextAlignment(.leading)
+    }
+
+    func hilightedText(str: String, searched: String) -> Text {
+        guard !str.isEmpty && !searched.isEmpty else { return Text(str) }
+
+        var result: Text!
+        let parts = str.components(separatedBy: searched)
+        for i in parts.indices {
+            result = (result == nil ? Text(parts[i]) : result + Text(parts[i]))
+            if i != parts.count - 1 {
+                result = result + Text(searched).bold()
+            }
+        }
+        return result ?? Text(str)
+    }
+}
+
+
+#Preview {
+    Lines()
 }
 
