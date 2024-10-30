0945fdb7e694baf9a5e75d120c6e68f125269bf0
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2024-10-30 09:30:00 +0700

    feat: create highlight helper view modifier

---
+++ hanvest/hanvest/Material/Presentation/Components/HighlightComponent/Model/Highlight.swift
@@ -0,0 +1,17 @@
+//
+//  Highlight.swift
+//  hanvest
+//
+//  Created by Bryan Vernanda on 30/10/24.
+//
+
+import SwiftUI
+
+//struct Highlight: Identifiable, Equatable, Hashable {
+//    var id: UUID = .init()
+//    var anchor: Anchor<CGRect>
+//    var title: String
+//    var cornerRadius: CGFloat
+//    var style: RoundedCornerStyle = .continuous
+//    var scale: CGFloat = 1
+//}
---
+++ hanvest/hanvest/Material/Presentation/Components/HighlightComponent/Model/HighlightAnchorKey.swift
@@ -0,0 +1,18 @@
+//
+//  HighlightAnchorKey.swift
+//  hanvest
+//
+//  Created by Bryan Vernanda on 30/10/24.
+//
+
+import SwiftUI
+
+//struct HighlightAnchorKey: PreferenceKey {
+//    static var defaultValue: [Int: Highlight] = [:]
+//    
+//    static func reduce(value: inout [Int: Highlight], nextValue: () -> [Int: Highlight]) {
+//        value.merge(nextValue()) { $1 }
+//    }
+//    
+//    
+//}
