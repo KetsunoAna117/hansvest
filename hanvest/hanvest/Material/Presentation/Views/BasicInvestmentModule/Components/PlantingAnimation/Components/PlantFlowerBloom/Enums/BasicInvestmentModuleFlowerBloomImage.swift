87e54fbb2a7e20ad7e91f956fe6e06f77e287b6d
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2024-11-14 12:18:43 +0700

    fix: refactor rightFlower position for plant flower bloom

--- hanvest/hanvest/Material/Presentation/Views/BasicInvestmentModule/Components/PlantingAnimation/Components/PlantFlowerBloom/Enums/BasicInvestmentModuleFlowerBloomImage.swift
+++ hanvest/hanvest/Material/Presentation/Views/BasicInvestmentModule/Components/PlantingAnimation/Components/PlantFlowerBloom/Enums/BasicInvestmentModuleFlowerBloomImage.swift
@@ -18,21 +18,21 @@ enum BasicInvestmentModuleFlowerBloomImage: CaseIterable {
     case leftFlower08
     case leftFlower09
     case leftFlower10
-//    case rightFlower01
-//    case rightFlower02
-//    case rightFlower03
-//    case rightFlower04
-//    case rightFlower05
-//    case rightFlower06
-//    case rightFlower07
-//    case rightFlower08
+    case rightFlower01
+    case rightFlower02
+    case rightFlower03
+    case rightFlower04
+    case rightFlower05
+    case rightFlower06
+    case rightFlower07
+    case rightFlower08
     
     var flowerImage: (image: Image, bottomPadding: CGFloat, leadingPadding: CGFloat) {
         switch self {
             case .leftFlower01:
-                (Image("flower-left-01"), 0, 0)
+                (Image("flower-left-01"), 12, -17)
             case .leftFlower02:
-                (Image("flower-left-02"), 70, -20)
+                (Image("flower-left-02"), 70, -25)
             case .leftFlower03:
                 (Image("flower-left-03"), 85, 88)
             case .leftFlower04:
@@ -49,31 +49,31 @@ enum BasicInvestmentModuleFlowerBloomImage: CaseIterable {
                 (Image("flower-left-09"), 293, 130)
             case .leftFlower10:
                 (Image("flower-left-10"), 226, 153)
-//            case .rightFlower01:
-//                (Image("flower-right-01"), -14.15, 153.55)
-//            case .rightFlower02:
-//                (Image("flower-right-02"), 395.76, 281.34)
-//            case .rightFlower03:
-//                (Image("flower-right-03"), 410.51, 308.42)
-//            case .rightFlower04:
-//                (Image("flower-right-04"), 426.73, 272.62)
-//            case .rightFlower05:
-//                (Image("flower-right-05"), 436.14, 221.11)
-//            case .rightFlower06:
-//                (Image("flower-right-06"), 453.69, 278.45)
-//            case .rightFlower07:
-//                (Image("flower-right-07"), 461.96, 243)
-//            case .rightFlower08:
-//                (Image("flower-right-08"), 500.89, 241.27)
+            case .rightFlower01:
+                (Image("flower-right-01"), -5, 320)
+            case .rightFlower02:
+                (Image("flower-right-02"), 55, 360)
+            case .rightFlower03:
+                (Image("flower-right-03"), 75, 410)
+            case .rightFlower04:
+                (Image("flower-right-04"), 105, 335)
+            case .rightFlower05:
+                (Image("flower-right-05"), 133, 233)
+            case .rightFlower06:
+                (Image("flower-right-06"), 177, 355)
+            case .rightFlower07:
+                (Image("flower-right-07"), 185, 273)
+            case .rightFlower08:
+                (Image("flower-right-08"), 265, 278)
         }
     }
     
     var halfAppleImage: (image: Image, bottomPadding: CGFloat, leadingPadding: CGFloat)?  {
         switch self {
             case .leftFlower01:
-                (Image("apple-left-01"), 0, 0)
+                (Image("apple-left-01"), 12, -17)
             case .leftFlower02:
-            (Image("flower-left-02"), 70, -20)
+            (Image("flower-left-02"), 70, -25)
             case .leftFlower03:
                 (Image("apple-left-03"), 85, 88)
             case .leftFlower04:
@@ -90,31 +90,31 @@ enum BasicInvestmentModuleFlowerBloomImage: CaseIterable {
                 (Image("flower-left-09"), 293, 130)
             case .leftFlower10:
                 (Image("apple-left-10"), 226, 153)
-//            case .rightFlower01:
-//                (Image("flower-right-01"), -8.23, 147.97)
-//            case .rightFlower02:
-//                (Image("apple-right-02"), 394.11, 280.54)
-//            case .rightFlower03:
-//                (Image("apple-right-03"), 403.62, 300.97)
-//            case .rightFlower04:
-//                (Image("apple-right-04"), 419.45, 264.91)
-//            case .rightFlower05:
-//                (Image("flower-right-05"), 436.14, 221.11)
-//            case .rightFlower06:
-//                (Image("flower-right-06"), 453.69, 278.45)
-//            case .rightFlower07:
-//                (Image("apple-right-07"), 457.08, 237.09)
-//            case .rightFlower08:
-//                (Image("apple-right-08"), 498.52, 237.42)
+            case .rightFlower01:
+                (Image("flower-right-01"), -5, 320)
+            case .rightFlower02:
+                (Image("apple-right-02"), 55, 360)
+            case .rightFlower03:
+                (Image("apple-right-03"), 75, 410)
+            case .rightFlower04:
+                (Image("apple-right-04"), 105, 335)
+            case .rightFlower05:
+                (Image("flower-right-05"), 133, 233)
+            case .rightFlower06:
+                (Image("flower-right-06"), 177, 355)
+            case .rightFlower07:
+                (Image("apple-right-07"), 185, 273)
+            case .rightFlower08:
+                (Image("apple-right-08"), 265, 278)
         }
     }
     
     var fullAppleImage: (image: Image, bottomPadding: CGFloat, leadingPadding: CGFloat)?  {
         switch self {
             case .leftFlower01:
-                (Image("apple-left-01"), 0, 0)
+                (Image("apple-left-01"), 12, -17)
             case .leftFlower02:
-            (Image("apple-left-02"), 70, -20)
+            (Image("apple-left-02"), 70, -25)
             case .leftFlower03:
                 (Image("apple-left-03"), 85, 88)
             case .leftFlower04:
@@ -131,22 +131,22 @@ enum BasicInvestmentModuleFlowerBloomImage: CaseIterable {
                 (Image("apple-left-09"), 293, 130)
             case .leftFlower10:
                 (Image("apple-left-10"), 226, 153)
-//            case .rightFlower01:
-//                (Image("apple-right-01"), -8.23, 147.97)
-//            case .rightFlower02:
-//                (Image("apple-right-02"), 394.11, 280.54)
-//            case .rightFlower03:
-//                (Image("apple-right-03"), 403.62, 300.97)
-//            case .rightFlower04:
-//                (Image("apple-right-04"), 419.45, 264.91)
-//            case .rightFlower05:
-//                (Image("apple-right-05"), 432.64, 216.91)
-//            case .rightFlower06:
-//                (Image("apple-right-06"), 457.13, 271.6)
-//            case .rightFlower07:
-//                (Image("apple-right-07"), 457.08, 237.09)
-//            case .rightFlower08:
-//                (Image("apple-right-08"), 498.52, 237.42)
+            case .rightFlower01:
+                (Image("apple-right-01"), -5, 320)
+            case .rightFlower02:
+                (Image("apple-right-02"), 55, 360)
+            case .rightFlower03:
+                (Image("apple-right-03"), 75, 410)
+            case .rightFlower04:
+                (Image("apple-right-04"), 105, 335)
+            case .rightFlower05:
+                (Image("apple-right-05"), 133, 233)
+            case .rightFlower06:
+                (Image("apple-right-06"), 177, 355)
+            case .rightFlower07:
+                (Image("apple-right-07"), 185, 273)
+            case .rightFlower08:
+                (Image("apple-right-08"), 265, 278)
         }
     }
 }
