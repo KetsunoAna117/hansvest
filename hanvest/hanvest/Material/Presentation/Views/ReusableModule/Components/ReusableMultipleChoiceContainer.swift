c02bed5d629a8afa969a865aeac58d2041ac436d
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2024-11-20 16:06:43 +0700

    Fix: question in quiz from Module 7 to have a better interface

--- hanvest/hanvest/Material/Presentation/Views/ReusableModule/Components/ReusableMultipleChoiceContainer.swift
+++ hanvest/hanvest/Material/Presentation/Views/ReusableModule/Components/ReusableMultipleChoiceContainer.swift
@@ -16,29 +16,32 @@ struct ReusableMultipleChoiceContainer: View {
     
     var body: some View {
         VStack(spacing: (!userPressSubmitButton) ? 48 : 0) {
-            VStack(spacing: 24) {
-                Text(data.question)
-                    .font(.nunito(.title2, .regular))
-                    .multilineTextAlignment(.center)
-                
-                if let imageName = data.imageName {
-                    Image(imageName)
+            ScrollView {
+                VStack(spacing: 24) {
+                    Text(data.question)
+                        .font(.nunito(.title2, .regular))
+                        .multilineTextAlignment(.center)
+                    
+                    if let imageName = data.imageName {
+                        Image(imageName)
+                    }
+                    
+                    ForEach(data.choices, id: \.self) { choice in
+                        HanvestButtonDefault(
+                            style: getButtonStyle(id: choice),
+                            title: choice,
+                            image: getButtonImage(id: choice),
+                            action: {
+                                self.selectedButtonID = choice
+                            }
+                        )
+                        .disabled(userPressSubmitButton)
+                    }
                 }
-                
-                ForEach(data.choices, id: \.self) { choice in
-                    HanvestButtonDefault(
-                        style: getButtonStyle(id: choice),
-                        title: choice,
-                        image: getButtonImage(id: choice),
-                        action: {
-                            self.selectedButtonID = choice
-                        }
-                    )
-                    .disabled(userPressSubmitButton)
-                }
+                .padding(.horizontal, 20)
+                .frame(maxHeight: .infinity, alignment: .top)
             }
-            .padding(.horizontal, 20)
-            .frame(maxHeight: .infinity, alignment: .top)
+            .scrollBounceBehavior(.basedOnSize, axes: [.vertical])
             
             if userPressSubmitButton {
                 if validateChoice() == true && selectedButtonID.isEmpty == false {
